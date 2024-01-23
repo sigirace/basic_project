417ca60e30c85c0810b59634346aad8470753954
SIGIRACE <chickang9@gmail.com>
2024-01-23 10:05:47 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,7 @@
-FROM --platform=linux/amd64 python:3.10.13-slim
-
-ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
-
-RUN apt-get update --fix-missing && \
-    apt-get install -y vim wget bzip2 curl git build-essential&& \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
-
-#  apt-get upgrade 
-#  python3-capstone libpq-dev 
-
-RUN mkdir /app
-WORKDIR /app
-
-COPY requirements.txt ./
-
-RUN pip install --upgrade pip
-RUN pip install --no-cache-dir -r requirements.txt
+FROM basic_project:0.0.1
 
 COPY app /app
 
-EXPOSE 80
+EXPOSE 8080
 
-# CMD ["uvicorn", "--host", "0.0.0.0", "--port", "80", "main:app"]
-# CMD ["tail", "-f", "/dev/null"]
-
-CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "main:app", "--bind", "0.0.0.0:80", "--workers", "4", "--threads", "4", "--timeout", "1000"]
+CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "main:app", "--bind", "0.0.0.0:8080", "--workers", "4", "--threads", "4", "--timeout", "1000"]
--- app/utils/mysqltools.py
+++ app/utils/mysqltools.py
@@ -7,17 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def create_pool(key):
+def create_pool():
 
     try:
-
-        # pymysqlpool.logger.setLevel('DEBUG')
-        # key = devkey
-        # aes = crypto.AESCipher(key=key)
         config = {
             'host': os.environ.get('HOST'),
             'user': os.environ.get('USERNAME'),
-            # 'password': aes.decrypt(os.environ.get('PASSWORD')),
             'password': os.environ.get('PASSWORD'),
             'database': os.environ.get('DB'),
             'port': int(os.environ.get('PORT')),
@@ -45,16 +40,69 @@ def execute_query(pool, sql, data):
     finally:
         con.close()
 
-def select(pool, sql, data):
+def select(pool, sql, args):
 
     con=pool.get_connection()
     cur = con.cursor()
 
     try:
-        cur.execute(sql, data)
+        cur.execute(sql, args)
         result = cur.fetchall()
     except Exception as e:
         raise e
     finally:
         con.close()
-    return result
+        return result
+
+def insert_dataframe(pool, table_name, dataframe):
+
+    con = pool.get_connection()
+    cur = con.cursor()
+
+    try:
+        con.begin()  # 트랜잭션 시작
+        for index, row in dataframe.iterrows():
+
+            columns = ', '.join(row.index)
+            placeholders = ', '.join(['%s'] * len(row))
+            sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+            args = tuple(row)
+
+            cur.execute(sql, args)
+
+        con.commit()  # 모든 삽입 작업이 성공하면 커밋
+
+    except Exception as e:
+        con.rollback()  # 삽입 중 예외가 발생하면 롤백
+        raise e
+
+    finally:
+        con.close()
+        
+def upsert_dataframe(pool, table_name, dataframe):
+
+    con = pool.get_connection()
+    cur = con.cursor()
+
+    try:
+        con.begin()  # 트랜잭션 시작
+        for index, row in dataframe.iterrows():
+            columns = ', '.join(row.index)
+            placeholders = ', '.join(['%s'] * len(row))
+            update_stmt = ', '.join([f"{col}=VALUES({col})" for col in row.index])
+
+            sql = f"""INSERT INTO {table_name} ({columns}) VALUES ({placeholders})
+                      ON DUPLICATE KEY UPDATE {update_stmt}"""
+
+            args = tuple(row)
+
+            cur.execute(sql, args)
+
+        con.commit()  # 모든 삽입/업데이트 작업이 성공하면 커밋
+
+    except Exception as e:
+        con.rollback()  # 작업 중 예외가 발생하면 롤백
+        raise e
+
+    finally:
+        con.close()
